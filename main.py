8451f9d3969198b04e71352ba3fce2aadeb0c947
Vuong Duong <80019487+vduong042@users.noreply.github.com>
2024-05-26 17:32:06 +0000

    Change date format and modify some functions

--- main.py
+++ main.py
@@ -1,32 +1,72 @@
 import os
 from pymongo.mongo_client import MongoClient
 
+
+# MongoDB URI từ biến môi trường
 uri = os.environ['uri']
 
-# Create a new client and connect to the server
+# Tạo client và kết nối tới server
 client = MongoClient(uri)
 
-# Send a ping to confirm a successful connection
+# Gửi lệnh ping để xác nhận kết nối thành công
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 
+
+
 from keepalive import keepalive
 from gnews import GNews
 import requests
 from bs4 import BeautifulSoup
-import newspaper
 import pandas as pd
 import time
 import schedule
 from datetime import datetime
+import pytz
 
+
+def get_website_url(url):
+    try:
+        response = requests.get(url, timeout=15, verify=False)
+        response.raise_for_status()  # Kiểm tra nếu có lỗi HTTP
+        return response.url
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching the website URL: {e}")
+        return None
+
+
+def get_website_description(url):
+    try:
+        response = requests.get(url, timeout=15, verify=False)
+        response.raise_for_status()  # Kiểm tra nếu có lỗi HTTP
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Các mục tiêu tìm kiếm mô tả
+        search_targets = [
+            ('meta', 'name', 'description'),
+            ('meta', 'property', 'og:description'),
+            ('div', 'class', 'description')
+        ]
+
+        # Tìm kiếm mô tả trong các mục tiêu
+        for tag, attr, value in search_targets:
+            description_tag = soup.find(tag, attrs={attr: value})
+            if description_tag:
+                if tag == 'meta' and 'content' in description_tag.attrs:
+                    return description_tag['content']
+                else:
+                    return description_tag.get_text(strip=True)
+
+        return 'Không có mô tả'
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching the article: {e}")
+        return None
 
 def main():
-    # Scraping news data
-    # Configurations
+    # Cấu hình scraping tin tức
     language = 'vi'
     country = 'Vietnam'
     period = '24h'
@@ -37,51 +77,10 @@ def main():
                     period=period,
                     max_results=max_results)
 
-    # Get top news
+    # Lấy tin tức hàng đầu
     news_data = gg_news.get_top_news()
 
-    # Get the final URL of the webpage
-    def get_website_url(url):
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                return response.url
-            else:
-                return None
-        except Exception as e:
-            return None
-
-    # Get the description of the webpage
-    def get_website_description(url):
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                if 'youtube.com' in response.url:
-                    return None
-
-                soup = BeautifulSoup(response.content, 'html.parser')
-
-                search_targets = [('meta', 'name', 'description'),
-                                  ('meta', 'property', 'og:description'),
-                                  ('div', 'class', 'description')]
-
-                for tag, attr, value in search_targets:
-                    description_tag = soup.find(tag, attrs={attr: value})
-                    if description_tag:
-                        if tag == 'meta' and 'content' in description_tag.attrs:
-                            return description_tag['content']
-                        else:
-                            return description_tag.get_text(strip=True)
-
-                return ('Không có mô tả')
-
-            else:
-                return None
-
-        except Exception as e:
-            return None
-
-    # Lists to store information of articles after parsing
+    # Danh sách để lưu thông tin bài báo sau khi phân tích
     title = []
     description = []
     url = []
@@ -89,16 +88,13 @@ def main():
     provider = []
     datePublished = []
 
-    # Process each article in the lists
+    # Xử lý từng bài báo trong danh sách
     for article in news_data:
         try:
             article_url = get_website_url(article["url"])
             article_description = get_website_description(article["url"])
-            # print(article_url)
 
-            if article_url != None and article_description != None:
-
-                # Extract information from the dictionary
+            if article_url and article_description:
                 title.append(article["title"])
                 description.append(article_description)
                 url.append(article_url)
@@ -109,7 +105,7 @@ def main():
         except Exception as e:
             print(f"Error processing JSON object: {e}")
 
-    # Combine the lists
+    # Kết hợp danh sách
     data = {
         'title': title,
         'description': description,
@@ -119,52 +115,61 @@ def main():
         'datePublished': datePublished
     }
 
-    # Create DataFrame
+    # print(data)
+
+    # Tạo DataFrame
     df = pd.DataFrame(data)
 
-    # Create or select the database
+    # Tạo hoặc chọn database
     db = client["news_database"]
 
-    # Create or select the collection
+    # Tạo hoặc chọn collection
     collection = db["news_collection"]
     scraper_info = db["scraper_info"]
 
-    # Convert DataFrame to list of dictionaries and insert into MongoDB
+    # Chuyển DataFrame thành danh sách dictionary và chèn vào MongoDB
     records = df.to_dict('records')
     total_article_count = 0
     duplicate_count = 0
     status = "Done"
 
+    # Định nghĩa múi giờ GMT +7
+    gmt_plus_7 = pytz.timezone('Asia/Bangkok')
+
+    # Chuyển đổi thời gian hiện tại sang múi giờ GMT +7
+    now_gmt_plus_7 = datetime.now().astimezone(gmt_plus_7)
+
+    # Định dạng chuỗi chỉ chứa thời gian
+    formatted_time = now_gmt_plus_7.strftime('%Y-%m-%d %H:%M:%S')
+
     try:
         for record in records:
             total_article_count += 1
-
             result = collection.update_one({"url": record["url"]},
                                            {"$setOnInsert": record},
                                            upsert=True)
-
             if result.matched_count > 0:
                 duplicate_count += 1
-
     except Exception as e:
         status = "Failed"
+        print(f"Error inserting into MongoDB: {e}")
 
     scraper_info.insert_one({
-        "Time": datetime.now(),
+        "Time": formatted_time,
         "Total articles": total_article_count,
         "Duplicate articles": duplicate_count,
         "Status": status
     })
 
-    print(f"{status}\nData scraping will continue in 3 hours.")
+    print(f"{status}\nData scraping will continue in 1 hours.")
 
 
 keepalive()
 
-# Scheduling the main function
+# Lên lịch chạy hàm main mỗi 1 giờ
 schedule.every(1).hour.do(main)
 
-# Start the loop to run on schedule.
+# Bắt đầu vòng lặp để chạy theo lịch trình
 while True:
     schedule.run_pending()
     time.sleep(1)
